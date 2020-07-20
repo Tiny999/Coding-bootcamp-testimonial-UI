1ece63a049ec875b40e33df5cd10b929f99f04b9
Tiny999 <salamiabbas3@gmail.com>
2020-07-20 22:47:19 +0100

    desktop version

---
+++ app.js
@@ -0,0 +1,44 @@
+function loadNext(){
+
+  document.getElementById('testimonial').innerHTML = `
+    <div id="testimonial">
+      <div class="text-container">
+        <p class="comment">"If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."</p>
+
+        <p class="name">John Tarkpor <span class="position">Junior Front-end Developer</span></p>
+      </div>
+      <div class="image">
+        <img src="./images/image-john.jpg" alt="john-image" id="main-img">
+
+
+        <div class="buttons">
+            <a href="#" id="previous" onclick="loadPrevious()" ><img src="./images/icon-prev.svg" alt="previous"></a>
+            <a href="#" id="next" onclick="loadNext()" ><img src="./images/icon-next.svg" alt="next"></a>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+
+function loadPrevious(){
+
+  document.getElementById('testimonial').innerHTML = `
+    <div id="testimonial" class="tanya">
+      <div class="text-container">
+        <p class="comment">"I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future."</p>
+
+        <p class="name">Tanya Sinclaire <span class="position">UX Engineer</span></p>
+      </div>
+      <div class="image">
+        <img src="./images/image-tanya.jpg" alt="tanya-image" id="main-img">
+
+
+        <div class="buttons">
+            <a href="#" id="previous" onclick="loadPrevious()" ><img src="./images/icon-prev.svg" alt="previous"></a>
+            <a href="#" id="next" onclick="loadNext()" ><img src="./images/icon-next.svg" alt="next"></a>
+        </div>
+      </div>
+    </div>
+  `;
+}
